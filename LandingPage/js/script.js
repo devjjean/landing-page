82d766de80f64d8d0c2b5a5cddc98bd6802f6300
Jean Sousa de Almeida <jjeanssousa16@gmail.com>
2024-11-01 15:51:16 -0300

    Atualização de todo o conteúdo do site

--- LandingPage/js/script.js
+++ LandingPage/js/script.js
@@ -1,6 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Espera um pouco antes de adicionar a classe loaded
+window.onload = function() {
+    const body = document.body;
+
+    // Adiciona a classe fade-in ao corpo
+    body.classList.add('fade-in');
+
+    // Define a opacidade como 1 para mostrar o conteúdo
     setTimeout(() => {
-        document.body.classList.add('loaded');
-    }, 50); // Atraso de 50 ms para garantir a transição
+        body.style.opacity = 1; // Revela todo o conteúdo
+    }, 100); // 100 ms de atraso
+};
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const navList = document.getElementById('nav-list');
+    navList.classList.toggle('active'); // Alterna a classe active
+});
+
+
+let currentIndex = 0;
+const intervalTime = 5000; // Tempo em milissegundos (3 segundos)
+
+function changeSlide(direction) {
+    const items = document.querySelectorAll('.carousel-item');
+    currentIndex = (currentIndex + direction + items.length) % items.length; // Loop circular
+
+    const offset = -currentIndex * 100; // Calcular o deslocamento
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+// Mudar automaticamente a cada intervalo
+let autoChange = setInterval(() => {
+    changeSlide(1); // Muda para o próximo item
+}, intervalTime);
+
+// Para pausar a mudança automática ao passar o mouse
+const carousel = document.querySelector('.carousel');
+
+carousel.addEventListener('mouseover', () => {
+    clearInterval(autoChange); // Para a mudança automática
+});
+
+carousel.addEventListener('mouseout', () => {
+    autoChange = setInterval(() => {
+        changeSlide(1); // Retorna a mudança automática
+    }, intervalTime);
 });
